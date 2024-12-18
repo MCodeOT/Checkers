d9b381ca0b03afeb9f7a8cadbfd81db56ae295da
zDaniiel <daniel-roselt@t-online.de>
2024-12-18 15:50:26 +0100

    created classes with attributes and basic methods

--- src/main/java/de/check/checkers/structures/Piece.java
+++ src/main/java/de/check/checkers/structures/Piece.java
@@ -1,4 +1,26 @@
 package de.check.checkers.structures;
 
+import de.check.checkers.utils.Position;
+
+
 public class Piece {
-}
+    private boolean isBlack;
+    private boolean isCrowned;
+    private byte id;
+    private Position currentPosition;
+
+    public Piece (boolean isBlack, byte id, Position currentPosition) {
+        this.isBlack = isBlack;
+        this.id = id;
+        this.currentPosition = currentPosition;
+    }
+
+    public boolean isBlack () { return isBlack; }
+    public boolean isCrowned() { return isCrowned; }
+    public void setCrowned(boolean isCrowned) { this.isCrowned = isCrowned; }
+    public Position getCurrentPosition() { return currentPosition; }
+    public void setCurrentPosition (Position currentPosition) { this.currentPosition = currentPosition; }
+
+
+    }
+
