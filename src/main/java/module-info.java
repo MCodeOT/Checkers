0ad6147830bd0b780efc43c25e151ea1506dbf1d
Steven-Dnn <dnn.steven05@gmail.com>
2024-12-18 15:16:17 +0100

    requirement for java.fxml deleted, so the "App" class launches normally

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,14 +1,9 @@
 module de.check.checkers {
     requires javafx.controls;
-    requires javafx.fxml;
     requires javafx.web;
 
-    opens de.check.checkers to javafx.fxml;
     exports de.check.checkers;
     exports de.check.checkers.ui;
-    opens de.check.checkers.ui to javafx.fxml;
     exports de.check.checkers.utils;
-    opens de.check.checkers.utils to javafx.fxml;
     exports de.check.checkers.structures;
-    opens de.check.checkers.structures to javafx.fxml;
 }
