a451f5b850b11da4d42760baf8d4c94ef937a231
MCodeOT <mcodeot@gmail.com>
2025-02-16 12:25:43 +0100

    Initialize Board class and implement constructor to set up a game board with pieces

--- src/main/java/de/check/checkers/structures/Board.java
+++ src/main/java/de/check/checkers/structures/Board.java
@@ -1,4 +1,36 @@
 package de.check.checkers.structures;
 
+import java.util.ArrayList;
+
 public class Board {
+
+    public Board() {
+
+        int size = 8;
+        ArrayList<Piece> board = new ArrayList<Piece>();
+        byte id = 0;
+        boolean isBlack = false;
+
+        for (int y = 0; y < size; y++) {
+
+            int oddAdjuster = y % 2;
+
+            for (int x = 0; x < size; x += 2) {
+
+                if (y != size / 2 || y != size / 2 - 1) {
+                    Position curPos = new Position(x + oddAdjuster, y);
+                    if (y < size/2 - 1) {
+                        isBlack = true;
+                    } else {
+                        isBlack = false;
+                    }
+                    board.add(new Piece(isBlack, id, curPos));
+                    id++;
+                }
+
+            }
+        }
+
+    }
+
 }
