365e486c0045cd56fb12024a55296ae58fb26998
zDaniiel <daniel-roselt@t-online.de>
2025-02-16 12:34:08 +0100

    created classes with attributes and basic methods

--- src/main/java/de/check/checkers/structures/Piece.java
+++ src/main/java/de/check/checkers/structures/Piece.java
@@ -9,14 +9,29 @@ public class Piece {
     private byte id;
     private Position currentPosition;
 
-    public Piece (boolean isBlack, byte id, Position currentPosition) {
+    public Piece(boolean isBlack, byte id, Position currentPosition) {
         this.isBlack = isBlack;
         this.id = id;
         this.currentPosition = currentPosition;
     }
 
-    public boolean isBlack () { return isBlack; }
-    public boolean isCrowned() { return isCrowned; }
-    public void setCrowned(boolean isCrowned) { this.isCrowned = isCrowned; }
-    public Position getCurrentPosition() { return currentPosition; }
-    public void setCurrentPosition (Position currentPosition) { this.currentPosition = currentPosition; }
+    public boolean isBlack() {
+        return isBlack;
+    }
+
+    public boolean isCrowned() {
+        return isCrowned;
+    }
+
+    public void setCrowned(boolean isCrowned) {
+        this.isCrowned = isCrowned;
+    }
+
+    public Position getCurrentPosition() {
+        return currentPosition;
+    }
+
+    public void setCurrentPosition(Position currentPosition) {
+        this.currentPosition = currentPosition;
+    }
+}
